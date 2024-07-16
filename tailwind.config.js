6e91d22d41f68d3978c7afd325698d9325ef4ec6
Chris Tam <chris-tam8@hotmail.com>
2024-07-16 11:31:58 -0700

    base commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: [
+    './*.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#0F5FAC',
+        blue: '#7ACFED',
+        red: '#EF4032',
+        yellow: '#FFC543',
+        gray: {
+          200: '#D6D6D6',
+          700: '#5E5E5E',
+        },
+      },
+      fontFamily: {
+        sans: ['Circular Std', 'Arial', 'sans-serif'],
+        display: ['Degular Display', 'Circular Std', 'Arial', 'sans-serif'],
+      },
+      backgroundImage: {
+        'hero-pattern': "url('/bg.png')",
+      },
+    },
+  },
+  plugins: [],
+}
